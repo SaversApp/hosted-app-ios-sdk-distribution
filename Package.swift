bdfffb7fda84bc6254880758e4692f76a7b7c9d6
ominto-ramesh <32033626+ominto-ramesh@users.noreply.github.com>
2026-04-17 11:48:58 +0530

    Update SaversAppSDK URL to new endpoint

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SaversAppSDK",
-            url: "https://faisalwahab.com/SaverAppIosSDK/SaversAppSDK.xcframework.zip",
+            url: "https://sdk.saversapp.com/ios/1.0.2/SaversAppSDK.xcframework.zip",
             checksum: "96d66be95bd96f87859a06bb5984a1c0951353940800cd41a9c6cfd9098bfe4d"
         )
     ]
